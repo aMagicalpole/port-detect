2b6f41da5295203a6d34f1be909491d1754e6fe0
wll8 <xwdaysme@gmail.com>
2024-09-27 13:25:16 +0800

    feat: 添加环境变量
    - feat: 可以在浏览器里调试
    - fix: 避免发布后通过 main.exe 启动时已经拥有 window.sys 时出现问题

--- src/main.js
+++ src/main.js
@@ -2,14 +2,32 @@ import 'element-plus/dist/index.css'
 import './assets/main.css'
 import ElementPlus from 'element-plus'
 
+import Sys from 'sys-shim'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-if (window.Sys) {
-  new window.Sys().then(async (main) => {
-    window.main = main
-    const app = createApp(App)
-    app.use(ElementPlus)
-    app.mount('#app')
-  })
+const app = createApp(App)
+app.use(ElementPlus)
+
+const fn = async sys => {
+  console.log(`初始化完成`, sys)
+  window.sys = sys
+  window.main = sys
+  window.ws = sys.ws
+  window.native = sys.native
 }
+
+// eslint-disable-next-line no-async-promise-executor
+new Promise(async () => {
+  if(window.sys) {
+    await fn(window.sys)
+  } else {
+    await new Sys({
+      log: true,
+      wsUrl: import.meta.env.VITE_SERVER_BASEURL || undefined,
+    }).then(fn).catch(err => {
+      console.error(`sys-shim 初始化失败`, err)
+    })
+  }
+  app.mount('#app')
+})
